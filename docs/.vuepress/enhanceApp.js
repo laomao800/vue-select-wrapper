776e3b5c9ce4163b0abe93641aa3924eeec4e021
laomao800-Mac <laomao800@qq.com>
2019-10-02 18:48:15 +0800

    docs: fix build error

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,18 +1,19 @@
-import Vue from 'vue'
-import DemoBox from '@laomao800/vue-demo-box'
-
-Vue.use(DemoBox, {
-  jsRes: [
-    '//unpkg.com/vue/dist/vue.js',
-    '//unpkg.com/@laomao800/vue-select-wrapper/dist/vue-select-wrapper.umd.min.js'
-  ]
-})
-
 export default ({ Vue }) => {
   import('../../src')
     .then(module => {
       const SelectWrapper = module.default
       Vue.use(SelectWrapper)
+    })
+    .catch(e => {})
+
+  import('@laomao800/vue-demo-box')
+    .then(module => {
+      Vue.use(DemoBox, {
+        jsRes: [
+          '//unpkg.com/vue/dist/vue.js',
+          '//unpkg.com/@laomao800/vue-select-wrapper/dist/vue-select-wrapper.umd.min.js'
+        ]
+      })
     })
     .catch(e => {})
 }
